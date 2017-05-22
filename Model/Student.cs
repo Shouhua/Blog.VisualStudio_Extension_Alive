689749105dc92978574e1898fccea87d77c31f9c
Echo Lee <bdottn.lee+git@gmail.com>
2017-05-23 01:40:00 +0800

    建立 Student 資料模型
    1.　加入屬性：Id
    2.　加入屬性：Name
    3.　加入屬性：Height
    4.　加入屬性：Weight

---
+++ Model/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace Model
+{
+    public sealed class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Height { get; set; }
+
+        public double Weight { get; set; }
+    }
+}
