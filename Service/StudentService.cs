dcacfbd2215a80cc2e8be33df9403e53607e09da
Echo Lee <bdottn.lee+git@gmail.com>
2017-05-23 01:42:44 +0800

    建立 StudentService
    1.　加入方法：CalculatorBMI
    2.　加入方法：StandardBMIPercentage

---
+++ Service/StudentService.cs
@@ -0,0 +1,33 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+
+namespace Service
+{
+    public sealed class StudentService
+    {
+        public double CalculatorBMI(Student student)
+        {
+            return student.Weight / Math.Pow(student.Height, 2);
+        }
+
+        public double StandardBMIPercentage(List<Student> students)
+        {
+            var standardStudents = new List<Student>();
+
+            foreach (var student in students)
+            {
+                var bmi = this.CalculatorBMI(student);
+
+                if (bmi >= 18.5 && bmi <= 24)
+                {
+                    standardStudents.Add(student);
+                }
+            }
+
+            var scale = (double)standardStudents.Count / (double)students.Count;
+
+            return scale * 100;
+        }
+    }
+}
